534b6cff7692b47b21b7679b54f4e06267d0af3f
Emil Staikov <emil@staikov.xyz>
2026-01-03 19:16:50 +0100

    fix: replaced flake-utils.lib.eachDefaultSystem with explicit forAllSystems pattern

--- minimal-flake/flake.nix
+++ minimal-flake/flake.nix
@@ -1,32 +1,31 @@
 {
   description = "A minimal development flake with nix language support.";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              nixd
-              # add packages needed in the development environment here
-            ];
-          };
 
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+      ];
+      forAllSystems =
+        function: genAttrs supportedSystems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixd
+          ];
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+    };
 }
